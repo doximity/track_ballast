9f13790672a3acc5a8156d1681c155051f1feebf
Benjamin Oakes <boakes@doximity.com>
2024-01-29 14:04:02 -0600

    Passing specs

--- spec/track_ballast_spec.rb
+++ spec/track_ballast_spec.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe TrackBallast do
   it "has a version number" do
     expect(TrackBallast::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
