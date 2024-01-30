4448cb56b7d8944894d5804a5d98337431f16543
Benjamin Oakes <boakes@doximity.com>
2024-01-30 11:43:27 -0600

    Document `TrackBallast.logger`

--- lib/track_ballast/logger.rb
+++ lib/track_ballast/logger.rb
@@ -2,6 +2,15 @@
 
 module TrackBallast
   class << self
+    # Internal logger for +TrackBallast+.
+    #
+    # This defaults to +Rails.logger+.  This +logger+ method may be removed in
+    # the future if we determine that +Rails.logger+ is more sensible given the
+    # focus of the library.  Please do not use +TrackBallast.logger+ outside of
+    # this gem.
+    #
+    # @!visibility private
+    # @return [ActiveSupport::TaggedLogging] a tagged logger
     attr_accessor :logger
   end
 end
