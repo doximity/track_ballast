45a90c8a03e324f76e2f4d185516bac95785cfa2
Benjamin Oakes <boakes@doximity.com>
2024-02-12 12:30:00 -0600

    Use stop_signal_key, StopSignalError, and document

--- lib/track_ballast/stop_signal.rb
+++ lib/track_ballast/stop_signal.rb
@@ -3,27 +3,140 @@
 require "track_ballast/redis"
 
 module TrackBallast
+  class StopSignalError < StandardError; end
+
+  # +StopSignal+ is a module that provides a way to stop a process while it is
+  # running.
+  #
+  # It uses Redis to store the stop signal.  Please see +TrackBallast.redis+
+  # for more details.
+  #
+  # When the stop signal is set, the client process will stop the next time the
+  # process manually checks the +stopped?+ method.
+  #
+  # == Usage
+  #
+  # +StopSignal+ can be used via +extend+ or +include+.
+  #
+  # === Extend
+  #
+  # - Extend the module in your class
+  # - Call +stopped?+ to check if the stop signal is set, for example: at the
+  #   beginning of the +perform+ method in a job class.
+  # - If desired, add an implementation of the +stop_signal_key+ method.  This
+  #   can be useful for stopping multiple classes with the same stop signal.  For
+  #   example: multiple related jobs.  The default implementation is to use the
+  #   class name.
+  #
+  # Then, from another process, you can manipulate the stop signal:
+  #
+  # - Call +YourClass.stop!+ to set the stop signal
+  # - Call +YourClass.go!+ to remove the stop signal
+  #
+  # ==== Example
+  #
+  #     class YourJob < ApplicationJob
+  #       extend TrackBallast::StopSignal
+  #
+  #       def perform
+  #         return if self.class.stopped?
+  #
+  #         # Process your job here
+  #       end
+  #
+  #       # Optional:
+  #       #
+  #       #     def self.stop_signal_key
+  #       #       "custom_stop_signal_key:stop"
+  #       #     end
+  #     end
+  #
+  # === Include
+  #
+  # - Include the module in your class
+  # - Call +stopped?+ to check if the stop signal is set, for example: at the
+  #   beginning of the +perform+ method in a job class.
+  # - If desired, add an implementation of the +stop_signal_key+ method.  This
+  #   can tie the stop signal to a specific instance of the class, as shown in
+  #   the example below.
+  #
+  # Then, from another process, you can manipulate the stop signal:
+  #
+  # - Call +your_model_instance.stop!+ to set the stop signal
+  # - Call +your_model_instance.go!+ to remove the stop signal
+  #
+  # ==== Example
+  #
+  #     class YourModel < ApplicationRecord
+  #       include TrackBallast::StopSignal
+  #
+  #       def stop_signal_key
+  #         "#{self.class.name}:#{id}:stop"
+  #       end
+  #     end
+  #
+  #     class YourModelService
+  #       def initialize(your_model_instance)
+  #         @your_model_instance = your_model_instance
+  #       end
+  #
+  #       def call
+  #         return if your_model_instance.stopped?
+  #
+  #         # Process `your_model_instance` here
+  #       end
+  #     end
+  #
   module StopSignal
+    # Set the stop signal
+    #
+    # @return [Boolean] +true+ if the stop signal was set
+    # @raise [StopSignalError] if an error occurred while setting the stop signal
     def stop!
-      redis.set(stop_key, true)
+      redis.set(stop_signal_key, true)
+
+      true
+    rescue Redis::CommandError
+      raise StopSignalError
     end
 
+    # Remove the stop signal
+    #
+    # @return [Boolean] +true+ if the stop signal was removed
+    # @raise [StopSignalError] if an error occurred while removing the stop signal
     def go!
-      redis.del(stop_key)
+      redis.del(stop_signal_key)
+
+      true
+    rescue Redis::CommandError
+      raise StopSignalError
     end
 
+    # Check if the stop signal is set
+    #
+    # @raise [StopSignalError] if an error occurred while checking the stop signal
     def stopped?
-      redis.exists(stop_key).positive?
+      redis.exists(stop_signal_key).positive?
+    rescue Redis::CommandError
+      raise StopSignalError
+    end
+
+    # The key used to store the stop signal.
+    #
+    # It defaults to the class name plus ":stop".
+    #
+    # This can be overridden to provide a custom key as described in the
+    # +StopSignal+ module documentation.
+    #
+    # @return [String] the key used to store the stop signal
+    def stop_signal_key
+      "#{name}:stop"
     end
 
     private
 
     def redis
       TrackBallast.redis
-    end
-
-    def stop_key
-      "#{name}:stop"
     end
   end
 end
--- spec/stop_signal_spec.rb
+++ spec/stop_signal_spec.rb
@@ -5,30 +5,151 @@ require "spec_helper"
 require "track_ballast/stop_signal"
 
 RSpec.describe TrackBallast::StopSignal do
-  it "has a lifecycle that allows stopping a job" do
-    fake_job = Class.new do
-      extend TrackBallast::StopSignal
+  describe "extend usage" do
+    it "has a lifecycle that allows stopping a job" do
+      fake_job = Class.new do
+        extend TrackBallast::StopSignal
+      end
+
+      # Default
+      expect(fake_job.stopped?).to eq(false)
+
+      fake_job.stop!
+
+      expect(fake_job.stopped?).to eq(true)
+
+      # Idempotent
+      fake_job.stop!
+
+      expect(fake_job.stopped?).to eq(true)
+
+      fake_job.go!
+
+      expect(fake_job.stopped?).to eq(false)
+
+      # Idempotent
+      fake_job.go!
+
+      expect(fake_job.stopped?).to eq(false)
     end
 
-    # Default
-    expect(fake_job.stopped?).to eq(false)
+    it "allows for multiple classes to share the same stop signal" do
+      fake_job_1 = Class.new do
+        extend TrackBallast::StopSignal
 
-    fake_job.stop!
+        def self.stop_signal_key
+          "shared_key:stop"
+        end
+      end
 
-    expect(fake_job.stopped?).to eq(true)
+      fake_job_2 = Class.new do
+        extend TrackBallast::StopSignal
 
-    # Idempotent
-    fake_job.stop!
+        def self.stop_signal_key
+          "shared_key:stop"
+        end
+      end
 
-    expect(fake_job.stopped?).to eq(true)
+      expect(fake_job_1.stopped?).to eq(false)
+      expect(fake_job_2.stopped?).to eq(false)
 
-    fake_job.go!
+      fake_job_1.stop!
 
-    expect(fake_job.stopped?).to eq(false)
+      expect(fake_job_1.stopped?).to eq(true)
+      expect(fake_job_2.stopped?).to eq(true)
 
-    # Idempotent
-    fake_job.go!
+      fake_job_1.go!
 
-    expect(fake_job.stopped?).to eq(false)
+      expect(fake_job_1.stopped?).to eq(false)
+      expect(fake_job_2.stopped?).to eq(false)
+    end
+  end
+
+  describe "include usage" do
+    it "has a lifecycle that allows stopping a model" do
+      fake_model = Class.new do
+        include TrackBallast::StopSignal
+
+        attr_reader :id
+
+        def initialize(id)
+          @id = id
+        end
+
+        def stop_signal_key
+          "#{self.class.name}:#{id}:stop"
+        end
+      end
+
+      fake_model_1 = fake_model.new(1)
+      fake_model_2 = fake_model.new(2)
+
+      # Default
+      expect(fake_model_1.stopped?).to eq(false)
+      expect(fake_model_2.stopped?).to eq(false)
+
+      fake_model_1.stop!
+
+      expect(fake_model_1.stopped?).to eq(true)
+      expect(fake_model_2.stopped?).to eq(false)
+
+      # Idempotent
+      fake_model_1.stop!
+
+      expect(fake_model_1.stopped?).to eq(true)
+      expect(fake_model_2.stopped?).to eq(false)
+
+      fake_model_1.go!
+
+      expect(fake_model_1.stopped?).to eq(false)
+      expect(fake_model_2.stopped?).to eq(false)
+
+      # Idempotent
+      fake_model_1.go!
+
+      expect(fake_model_1.stopped?).to eq(false)
+      expect(fake_model_2.stopped?).to eq(false)
+    end
+  end
+
+  describe "error handling" do
+    it "raises a wrapped error when an error occurs while setting the stop signal" do
+      fake_job = Class.new do
+        extend TrackBallast::StopSignal
+      end
+
+      allow(TrackBallast.redis).to receive(:set).and_raise(Redis::CommandError)
+
+      expect { fake_job.stop! }
+        .to raise_error(TrackBallast::StopSignalError) do |error|
+          expect(error.cause).to be_kind_of(Redis::CommandError)
+        end
+    end
+
+    it "raises a wrapped error when an error occurs while removing the stop signal" do
+      fake_job = Class.new do
+        extend TrackBallast::StopSignal
+      end
+
+      allow(TrackBallast.redis).to receive(:del).and_raise(Redis::CommandError)
+
+      expect { fake_job.go! }
+        .to raise_error(TrackBallast::StopSignalError) do |error|
+          expect(error.cause).to be_kind_of(Redis::CommandError)
+        end
+    end
+
+    it "raises a wrapped error when an error occurs while checking the stop signal" do
+      fake_job = Class.new do
+        extend TrackBallast::StopSignal
+      end
+
+      allow(TrackBallast.redis).to receive(:exists).and_raise(Redis::CommandError)
+
+      expect { fake_job.stopped? }
+        .to raise_error(TrackBallast::StopSignalError) do |error|
+          expect(error.cause).to be_kind_of(Redis::CommandError)
+        end
+    end
   end
 end
