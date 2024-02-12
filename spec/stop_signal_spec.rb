eb98dc285ca7b04fbf6a6522596634111e670f92
Benjamin Oakes <boakes@doximity.com>
2024-02-12 11:35:15 -0600

    Add TrackBallast::StopSignal

---
+++ spec/stop_signal_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+require "track_ballast/stop_signal"
+
+RSpec.describe TrackBallast::StopSignal do
+  it "has a lifecycle that allows stopping a job" do
+    fake_job = Class.new do
+      extend TrackBallast::StopSignal
+    end
+
+    # Default
+    expect(fake_job.stopped?).to eq(false)
+
+    fake_job.stop!
+
+    expect(fake_job.stopped?).to eq(true)
+
+    # Idempotent
+    fake_job.stop!
+
+    expect(fake_job.stopped?).to eq(true)
+
+    fake_job.go!
+
+    expect(fake_job.stopped?).to eq(false)
+
+    # Idempotent
+    fake_job.go!
+
+    expect(fake_job.stopped?).to eq(false)
+  end
+end
